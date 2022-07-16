db4138c0a539cf1928a14a04d7e5b4ce18580763
Agneev Mukherjee <19761269+agneevX@users.noreply.github.com>
2022-07-16 16:58:46 +0530

    Add mtr functionality

--- src/main/java/io/github/johannesschaefer/webnettools/payload/MtrPayload.java
+++ src/main/java/io/github/johannesschaefer/webnettools/payload/MtrPayload.java
@@ -9,11 +9,32 @@ import lombok.Data;
 @Data
 @Tool(name="mtr", displayName="MTR", cmd="mtr", description="Network diagnostic tool combining 'traceroute' and 'ping'")
 public class MtrPayload implements Payload {
-    @MainParameter(displayName ="Hostname")
+    @MainParameter(displayName ="Hostname or IP address", description="Specify a hostname or an IP address")
     private String hostname;
 
-    @FixedParam(param = "--report", paramType = ParameterType.ONLY_PARAM)
+    @FixedParam(param = "--report-wide", paramType = ParameterType.ONLY_PARAM)
     private Boolean report = true;
+    
+    @NumberParam(displayName = "Pings", param="--report-cycles", min=1., max=1000., step=1., description = "Set the number of pings sent")
+    private Integer pings;
+       
+    @BooleanParam(displayName = "Use UDP", param = "--udp", paramType = ParameterType.ONLY_PARAM, description = "Use UDP instead of ICMP echo")
+    private Boolean useUdp;
+    
+    @BooleanParam(displayName = "Use TCP", param = "--tcp", paramType = ParameterType.ONLY_PARAM, description = "Use TCP instead of ICMP echo")
+    private Boolean useTcp;
+    
+    @NumberParam(displayName = "Port", param="--port", min=1., max=65536., step=1., description = "Target port number for TCP, SCTP, or UDP")
+    private Integer port;
+    
+    @BooleanParam(displayName = "No DNS", param = "--no-dns", paramType = ParameterType.ONLY_PARAM, description = "Do not resolve host names")
+    private Boolean noDns;
+    
+    @BooleanParam(displayName = "AS Lookup", param = "--as-lookup", paramType = ParameterType.ONLY_PARAM, description = "Display AS number")
+    private Boolean asLookup;
+    
+    @BooleanParam(displayName = "Show IPs", param = "--show-ips", paramType = ParameterType.ONLY_PARAM, description = "Show IP numbers and host names")
+    private Boolean showIps;
 
     @Override
     public String getCacheString() {
